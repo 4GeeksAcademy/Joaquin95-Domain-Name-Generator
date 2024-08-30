1b067bee8db88b8eb730297b86d090ac4b40ed96
Joaquin Morales <Mintinvestments95@gmail.com>
2024-08-30 20:49:25 +0000

    Need to finish

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(domain) {
+  let pronoun = ["the", "our", "power", "my", "what"];
+  let adj = ["morales", "awesome", "greatness", "delicious", "funny"];
+  let noun = ["Pizza", "dog", "lift", "gym", "car"];
+  let domainextensions = [".com", ".org", ".net", ".mx"];
+
+  console.log(pronoun[0] + adj[0] + noun[0] + domainextensions[0]);
 };
