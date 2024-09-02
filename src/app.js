d6b9ec6fa7fec574cf2465c1bddaa0c71da33fc8
Joaquin Morales <Mintinvestments95@gmail.com>
2024-09-02 23:59:59 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -11,10 +11,22 @@ window.onload = function() {
   let noun = ["Pizza", "dog", "lift", "gym", "car"];
   let extensions = [".com", ".org", ".net", ".mx", ".us"];
 
+  let domainNames = [];
+
   for (let i = 0; i < pronoun.length; i++) {
     for (let a = 0; a < adj.length; a++)
       for (let n = 0; n < noun.length; n++)
         for (let e = 0; e < extensions.length; e++)
-          console.log(pronoun[i] + adj[a] + noun[n] + extensions[e]);
+          domainNames.push(pronoun[i] + adj[a] + noun[n] + extensions[e]);
   }
+
+  document.body.innerHTML = "<ul></ul>";
+
+  let ul = document.querySelector("ul");
+
+  domainNames.forEach(function(domain) {
+    let li = document.createElement("li");
+    li.textContent = domain;
+    ul.appendChild(li);
+  });
 };
